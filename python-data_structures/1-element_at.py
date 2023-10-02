8c733dd7adc9e3ae359ec22a944c0e49b4ddaf18
Ergi004 <ergilama98@gmail.com>
2023-10-02 05:49:49 -0700

    element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,5 @@ def element_at(my_list, idx):
     for i in my_list:
         if i == idx:
             return my_list[idx]
-        if idx < 0:
-            return None
-        if idx > len(my_list):
+        if idx < 0 & idx >= len(my_list):
             return None
