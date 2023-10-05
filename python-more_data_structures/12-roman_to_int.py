db91b70e71aab6884bedbc02e9bed51a6e435061
Ergi004 <ergilama98@gmail.com>
2023-10-05 03:07:45 -0700

    Turnin into roman

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string != str(roman_string) or roman_string == None:
+        return 0
+    romanNum = {
+            'I': 1 , 'V': 5, 'X': 10,'L': 50, 'C': 100,'D': 500,'W': 1000
+            }
+    value = 0
+    for i in range(len(roman_string) - 1):
+        if romanNum[roman_string[i]] < romanNum[roman_string[i + 1]]:
+            value -= romanNum[roman_string[i]]
+        else:
+            value += romanNum[roman_string[i]]
+    value += romanNum[roman_string[-1]]
+    return value
