83729c45a73825c9b33ba3ee064cba1d76884306
Ergi004 <ergilama98@gmail.com>
2023-10-05 06:30:44 -0700

    Division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,9 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(d))
     return d
-safe_print_division = __import__('3-safe_print_division').safe_print_division
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
