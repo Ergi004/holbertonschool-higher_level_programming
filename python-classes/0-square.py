d4e90edae69036b2efcf1d9eef12e8d8764dc439
Ergi004 <ergilama98@gmail.com>
2023-10-09 02:02:56 -0700

    Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,5 +5,6 @@ python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 python3 -c 'print(__import__("my_module").my_function.__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
 """
+
 class Square:
     pass
