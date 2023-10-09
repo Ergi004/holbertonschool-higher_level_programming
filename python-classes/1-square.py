a0ca363f3b4c49ad162c11a69b639a51b6d85eb2
Ergi004 <ergilama98@gmail.com>
2023-10-09 02:52:02 -0700

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,6 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 python3 -c 'print(__import__("my_module").my_function.__doc__)'
-python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
 """
 
 
