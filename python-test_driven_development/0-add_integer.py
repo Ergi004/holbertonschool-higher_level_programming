a977c540ab20fb5c8a382c9ef2df65211c08649b
Ergi004 <ergilama98@gmail.com>
2023-10-10 03:27:01 -0700

    s

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if isinstance(a, int) is False and isinstance(a, float) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, int) is False and isinstance(b, float) is False:
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return int(a) + int(b)
