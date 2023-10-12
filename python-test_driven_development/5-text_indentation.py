23c5d42a6aec13006640a3d530839ee826bd7395
Ergi004 <ergilama98@gmail.com>
2023-10-12 06:07:53 -0700

    Fixed?

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-python3 -c 'print(__import__("my_module").__doc__)'
-"""
+"""Text indentation module"""
 
 
 def text_indentation(text):
-    """
-    python3 -c 'print(__import__("my_module").my_function.__doc__)'
-    """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+    """Indents text whenever needed"""
+    if type(text) is not str:
+        raise TypeError("text must be string")
+    char = ['.', '?', ':']
     i = 0
-    length = len(text)
-    while i < length:
-        print(text[i], end='')
-        if text[i] in ['.', '?', ':']:
-            print("\n\n", end='')
-            i += 1
-            while i < length and text[i] == ' ':
+    while i in range(len(text)):
+        print('{}'.format[textp[i], end="")
+        if text[[i] in char:
+            print('\n')
+            while text[i + 1] == " ":
                 i += 1
-            continue
         i += 1
