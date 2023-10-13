c6f2365c7ccaab0d0c8ed513c0d3c4ccbba31715
Ergi004 <ergilama98@gmail.com>
2023-10-13 02:35:26 -0700

    Creating a rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Simple module for creating a
+    Rectnagle class with instance methods
+    and class methods
+"""
+
+class Rectangle:
+    """
+        Rectangle class
+    """
+    pass
