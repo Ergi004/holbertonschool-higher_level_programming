5fe56b776bc237b5f7de58321a76e47eaeb53f7c
Ergi004 <ergilama98@gmail.com>
2023-10-13 02:13:59 -0700

    Tests

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Text indentation module"""
+
+'''
+
+Module to print a given text
+and insterting newlines when encountaring
+this type of characters . ? !
+'''
 
 
 def text_indentation(text):
-    """Indents text whenever needed"""
+    '''
+
+    Function that prints the text given as argument
+    and inserting newlines when encountaring this
+    type of characters . ? !
+    '''
+
     if type(text) is not str:
-        raise TypeError("text must be string")
-    char = ['.', '?', ':']
+        raise TypeError("text must be a string")
+
     i = 0
-    while i in range(len(text)):
-        print('{}'.format[textp[i], end="")
-        if text[[i] in char:
-            print('\n')
-            while text[i + 1] == " ":
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ['.', '?', ':']:
+            print("\n\n", end='')
+            i += 1
+            while i < len(text) and text[i] == ' ':
                 i += 1
+            continue
         i += 1
