ec749240ccd388229fc6a16b46b1ca3db26c681a
Ergi004 <ergilama98@gmail.com>
2023-10-17 05:31:57 -0700

    Sorting

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,16 +3,13 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 """
 
+
 class MyList(list):
     """
     python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
 
     def print_sorted(self):
-        """
-        python3 -c 'print(__import__("my_module").my_function.__doc__)'
-        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-        """
         ls = self.copy()
         ls.sort()
         print(ls)
