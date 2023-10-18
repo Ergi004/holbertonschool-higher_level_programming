323ead5210d2f517ceca7c8e0c51b8cbb09db5a9
Ergi004 <ergilama98@gmail.com>
2023-10-18 11:59:26 -0700

    Task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
