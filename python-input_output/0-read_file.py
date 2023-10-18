7727932787e0e9e3fe56877754df26a315416b2e
Ergi004 <ergilama98@gmail.com>
2023-10-18 05:48:10 -0700

    Reading file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def read_file(filename=""):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
     with open("my_file_0.txt", encoding="utf-8") as f:
         read_data = f.read()
