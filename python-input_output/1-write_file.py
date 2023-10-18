4fc79976175e17361b3c19e52cfe2de64890543a
Ergi004 <ergilama98@gmail.com>
2023-10-18 06:54:41 -0700

    Writing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -10,5 +10,5 @@ def write_file(filename="", text=""):
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """
 
-    with open(filename, 'w', encoding='utf-8') as f: 
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
