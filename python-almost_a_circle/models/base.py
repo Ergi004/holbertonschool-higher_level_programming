06c8fbb2e5dd07b350b6d78e48a764b9277533d0
Ergi Lama <ergilama98@gmail.com>
2023-10-23 04:26:10 -0700

    Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Base module
+"""
+
+
+class Base:
+    """
+    Class Base
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.__id = id
+
