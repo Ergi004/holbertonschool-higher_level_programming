5b3d17a2e5e7f876bbcee1a349890ce39b6ccfe7
Ergi Lama <ergilama98@gmail.com>
2023-10-25 05:51:28 -0700

    task 10

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,11 +38,11 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.__height = value
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
