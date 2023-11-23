48b7b12bed5601e38e8843dc1d9147bac7ee595e
Ergi Lama <ergilama98@gmail.com>
2023-11-23 01:11:05 -0800

    4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT NOT NULL,
-	id INT DEFAULT 1,
-	name VARCHAR(256)
-	);
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
