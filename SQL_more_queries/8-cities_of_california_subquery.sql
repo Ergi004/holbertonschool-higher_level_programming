29a140b79a086932bc5bbbab2b2606ff5c6fef39
Ergi Lama <ergilama98@gmail.com>
2023-11-23 02:41:48 -0800

    Task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,14 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        state_id INT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        FOREIGN KEY(state_id) REFERENCES states(id)
+        );
+SELECT cities.id, cities.name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
+
