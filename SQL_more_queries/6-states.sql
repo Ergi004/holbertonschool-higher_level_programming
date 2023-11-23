25cccbf837c8d148b55f5219a64b3ce8eb18587d
Ergi Lama <ergilama98@gmail.com>
2023-11-23 01:25:52 -0800

    states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256)
+);
