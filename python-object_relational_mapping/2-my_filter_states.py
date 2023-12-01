3ebee5a3b744587220d29255697c07f8338cb200
Ergi Lama <ergilama98@gmail.com>
2023-12-01 01:21:03 -0800

    arizona

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+Write a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 """
 
 
 import MySQLdb
 import sys
+
 
 def get_states(username, password, db_name, state_name):
     db = MySQLdb.connect(
@@ -26,6 +28,6 @@ def get_states(username, password, db_name, state_name):
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     get_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-
