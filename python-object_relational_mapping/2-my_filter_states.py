678c3bcfe81ba86862724cf438678f14864b4165
Ergi Lama <ergilama98@gmail.com>
2023-12-01 01:18:52 -0800

    arizona

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-List all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
+Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 """
+
 
 import MySQLdb
 import sys
